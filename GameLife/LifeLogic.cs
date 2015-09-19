438a1fc77859605563bfc8650fa7354752901f69
Ivan Erokhin <wancheez@gmail.com>
2015-09-19 19:05:59 +0300

    First Commit

---
+++ GameLife/LifeLogic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GameLife
+{
+    public class LifeLogic
+    {
+        public bool[,] Universe;
+        int xNum, yNum;
+
+        public bool[,] UniverseArray
+        {
+            get {return Universe;}
+        }
+
+        public LifeLogic(int x, int y, bool[,]Univ)
+        {
+            xNum = x;
+            yNum = y;
+            Universe = Univ;
+        }
+
+        public Color RedColor 
+        {
+            get { return Colors.Red; }
+        }
+
+        public void nextStep()
+        {
+            for (int i = 1; i < xNum-1; i++)
+            {
+                for (int j = 1; j < yNum-1; j++)
+                {
+                    if ( (!Universe[i,j] && checkNeighbors(i, j) == 3) || 
+                        (Universe[i,j] && (checkNeighbors(i, j) == 3 || checkNeighbors(i, j) == 2)))
+                    {
+                        Universe[i,j] = true; //Клетка оживает или остается живой
+                    } else
+                    {
+                        Universe[i,j] = false;
+                    }
+                }
+            }
+        }
+
+        private int checkNeighbors(int i, int j)
+        {
+            int counter = 0;
+            counter += (Universe[i + 1,j]) ? 1 : 0;
+            counter += (Universe[i + 1,j - 1]) ? 1 : 0;
+            counter += (Universe[i + 1,j + 1]) ? 1 : 0;
+            counter += (Universe[i - 1,j]) ? 1 : 0;
+            counter += (Universe[i - 1,j + 1]) ? 1 : 0;
+            counter += (Universe[i - 1,j - 1]) ? 1 : 0;
+            counter += (Universe[i,j + 1]) ? 1 : 0;
+            counter += (Universe[i,j - 1]) ? 1 : 0;
+            return counter;
+        }
+
+    }
+}
---
+++ GameLife/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameLife
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            bool[,] testarray = new bool[,] {{false, false, false, false, false, false, false},
+                                            {false, false, false, true, false, false, false},
+                                            {false, false, true, false, false, false, false},
+                                            {false, true, true, true, false, false, false},
+                                            {false, false, false, false, false, false, false},
+                                            {false, false, false, false, false, false, false},
+                                            {false, false, false, false, false, false, false}};
+
+            LifeLogic ll = new LifeLogic(7, 7, testarray);
+            //MainGrid.ItemsSource2D = ll.Universe;
+            
+            MainGrid.DataContext = ll;
+            
+        }
+    }
+}
+    
